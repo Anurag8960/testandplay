0d04e8ae3331a05f289f07f70e827de65720f463
Anurag8960 <anurags05072000@gmail.com>
2024-03-31 11:26:59 +0530

    hello.js modify

--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
-Hello How are you
+    // Check if response is JSON
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Response is not in JSON format');
+    }
